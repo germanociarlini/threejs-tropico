644d369748404ae52bc62db43da355e4c08b64a8
Germano Ciarlini <germano.ciarlini@gmail.com>
2021-04-11 12:32:55 -0300

    Refactor header style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,12 +5,24 @@ export class Header extends React.Component {
   render() {
     return (
       <header className='header'>
-        <div className="logo">Tropico</div>
-        <div className="header-sections">
-          <a className="header-section" href="#home">home</a>
-          <a className="header-section" href="#destinations">destinations</a>
-          <a className="header-section" href="#about">tropico</a>
-          <a className="header-section" href="#contact">contact</a>
+        <div className="header-container">
+          <div className="logo">Tropico</div>
+          <nav className="header-sections">
+            <ul className="header-sections-list">
+              <li>
+                <a className="header-section" href="#home">home</a>
+              </li>
+              <li>
+                <a className="header-section" href="#destinations">destinations</a>
+              </li>
+              <li>
+                <a className="header-section" href="#about">tropico</a>
+              </li>
+              <li>
+                <a className="header-section" href="#contact">contact</a>
+              </li>
+            </ul>
+          </nav>
         </div>
       </header>
     )
