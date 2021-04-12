d60206b34d2133c4ec575da1f89188d1dce51a0d
Germano Ciarlini <germano.ciarlini@gmail.com>
2021-04-12 11:46:02 -0300

    Restrain content width to header and add about and contact components

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -2,17 +2,27 @@ import React from 'react';
 import '../styles/Content.css';
 import { Home } from './Home';
 import { Destinations } from './Destinations';
+import { About } from './About';
+import { Contact } from './Contact';
 
 export class Content extends React.Component {
   render() {
     return (
       <main className='content'>
         <section id='home' className='fwh-slide'>
-          <Home />
+          <div className="section-content"><Home /></div>
         </section>
 
         <section id='destinations' className='fwh-slide'>
-          <Destinations />
+          <div className="section-content"><Destinations /></div>
+        </section>
+
+        <section id='about' className='fwh-slide'>
+          <div className="section-content"><About /></div>
+        </section>
+
+        <section id='contact' className='fwh-slide'>
+          <div className="section-content"><Contact /></div>
         </section>
       </main>
     )
